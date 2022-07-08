b29529acf18640d51590a88cc1e417900aef3b95
jenishadholariya <jenishadholariya5@gmail.com>
2022-07-08 16:17:49 +0530

    navidator

--- src/Container/Appoinment/Bookappointment.js
+++ src/Container/Appoinment/Bookappointment.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import * as yup from 'yup';
 import { Formik, useFormik, Form } from 'formik';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 
 function Bookappointment(props) {
-    // const history=useHistory();
+
+    const history = useHistory();
+    const [update,setUpdate] =useState(false);
 
     let schema = yup.object().shape({
         name: yup.string().required("Enter your name"),
@@ -15,7 +17,9 @@ function Bookappointment(props) {
         date: yup.date().required("Enter Appointment date"),
     });
 
-    const handleinsert = (values) => {
+    const handleInsert = (values) => {
+
+        let localData = JSON.parse(localStorage.getItem("apt"));
 
         let id=Math.floor(Math.random()*1000);
 
@@ -24,20 +28,55 @@ function Bookappointment(props) {
             ...values
         }
 
-        let LocalData=JSON.parse(localStorage.getItem("apt"));
 
-        if(LocalData === null){
+        if(localData === null){
             localStorage.setItem("apt",JSON.stringify([data]));
         }
         else{
-            LocalData.push(data);
-            localStorage.setItem("apt",JSON.stringify(LocalData));
+            localData.push(data);
+            localStorage.setItem("apt",JSON.stringify(localData));
         }
 
-        console.log(LocalData);
+        history.push("/Listappointment");
+        console.log(localData);
     }
 
-    // history.push('/Listappointment');
+    const handleUpadate=(values)=>{
+
+        let localData=JSON.parse(localStorage.getItem("apt"));
+
+        let uData=localData.map((d)=>{
+            if(d.id === values.id){
+                return values;
+            }else{
+                return d;
+            }
+        })
+
+        localStorage.setItem("apt",JSON.stringify(uData));
+
+        history.replace();
+        setUpdate(false);
+        formik.resetForm();
+        history.push('/Listappointment')
+
+    }
+
+    useEffect(() => {
+
+        let localData=JSON.parse(localStorage.getItem("apt"));
+
+        if(props.location.state && localData !== null){
+            let fData=localData.filter((d)=>d.id === props.location.state.id);
+
+            formik.setValues(fData[0]);
+
+            setUpdate(true);
+        }
+        
+    }, []);
+
+    // history.push("/Listappointment");
 
     const formik = useFormik({
         initialValues: {
@@ -52,12 +91,18 @@ function Bookappointment(props) {
         validationSchema: schema,
 
         onSubmit: values => {
-            handleinsert(values);
+
+            if(update){
+                handleUpadate(values);
+            }else{
+                handleInsert(values);
+            }
+            
         },
 
     });
 
-    const { handleChange, errors, handleSubmit, handleBlur, touched } = formik;
+    const { handleChange, errors, handleSubmit, handleBlur, touched ,values} = formik;
 
     return (
         <main id="main">
@@ -78,29 +123,85 @@ function Bookappointment(props) {
                         <Form className="php-email-form" onSubmit={handleSubmit}>
                             <div className="row">
                                 <div className="col-md-4 form-group">
-                                    <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" onChange={handleChange} onBlur={handleBlur} />
-                                    <p className="text-danger">{errors.name && touched.name ? errors.name : ''}</p>
+                                    <input 
+                                    type="text" 
+                                    name="name" 
+                                    value={values.name}
+                                    className="form-control" 
+                                    id="name" placeholder="Your Name" 
+                                    data-rule="minlen:4" data-msg="Please enter at least 4 chars" 
+                                    onChange={handleChange} 
+                                    onBlur={handleBlur} 
+                                    />
+                                    <p className="text-danger">
+                                        {errors.name && touched.name ? errors.name : ''}
+                                        </p>
                                     <div className="validate" />
                                 </div>
                                 <div className="col-md-4 form-group mt-3 mt-md-0">
-                                    <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" onChange={handleChange} onBlur={handleBlur} />
-                                    <p className="text-danger">{errors.email && touched.email ? errors.email : ''}</p>
+                                    <input 
+                                    type="email" 
+                                    className="form-control" 
+                                    name="email" 
+                                    value={values.email}
+                                    id="email" 
+                                    placeholder="Your Email" 
+                                    data-rule="email" 
+                                    data-msg="Please enter a valid email" 
+                                    onChange={handleChange} 
+                                    onBlur={handleBlur} 
+                                    />
+                                    <p className="text-danger">
+                                        {errors.email && touched.email ? errors.email : ''}
+                                        </p>
                                     <div className="validate" />
                                 </div>
                                 <div className="col-md-4 form-group mt-3 mt-md-0">
-                                    <input type="tel" className="form-control" name="phone" id="phone" placeholder="Your Phone" data-rule="minlen:4" data-msg="Please enter at least 4 chars" onChange={handleChange} onBlur={handleBlur} />
-                                    <p className="text-danger">{errors.phone && touched.phone ? errors.phone : ''}</p>
+                                    <input 
+                                    type="tel" 
+                                    className="form-control" 
+                                    name="phone" 
+                                    id="phone" 
+                                    value={values.phone}
+                                    placeholder="Your Phone" 
+                                    data-rule="minlen:4" 
+                                    data-msg="Please enter at least 4 chars" 
+                                    onChange={handleChange} 
+                                    onBlur={handleBlur} 
+                                    />
+                                    <p className="text-danger">
+                                        {errors.phone && touched.phone ? errors.phone : ''}
+                                        </p>
                                     <div className="validate" />
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="col-md-4 form-group mt-3">
-                                    <input type="datetime" name="date" className="form-control datepicker" id="date" placeholder="Appointment Date" data-rule="minlen:4" data-msg="Please enter at least 4 chars" onChange={handleChange} onBlur={handleBlur} />
-                                    <p className="text-danger">{errors.date && touched.date ? errors.date : ''}</p>
+                                    <input 
+                                    type="date" 
+                                    name="date" 
+                                    className="form-control datepicker" 
+                                    id="date" 
+                                    value={values.date}
+                                    placeholder="Appointment Date" 
+                                    data-rule="minlen:4" 
+                                    data-msg="Please enter at least 4 chars"
+                                    onChange={handleChange} 
+                                    onBlur={handleBlur} 
+                                    />
+                                    <p className="text-danger">
+                                        {errors.date && touched.date ? errors.date : ''}
+                                    </p>
                                     <div className="validate" />
                                 </div>
                                 <div className="col-md-4 form-group mt-3 ">
-                                    <select name="department" id="department" className="form-select" onChange={handleChange} onBlur={handleBlur}>
+                                    <select 
+                                    name="department" 
+                                    id="department" 
+                                    className="form-select" 
+                                    onChange={handleChange} 
+                                    onBlur={handleBlur}>
+                                        value={values.department}
                                         <option value>Select Department</option>
                                         <option value="Department 1">Department 1</option>
                                         <option value="Department 2">Department 2</option>
@@ -111,7 +212,16 @@ function Bookappointment(props) {
                                 </div>
                             </div>
                             <div className="form-group mt-3">
-                                <textarea className="form-control" name="message" rows={5} placeholder="Message (Optional)" defaultValue={""} onChange={handleChange} onBlur={handleBlur} />
+                                <textarea 
+                                className="form-control" 
+                                name="message" 
+                                rows={5} 
+                                value={values.message}
+                                placeholder="Message (Optional)" 
+                                defaultValue={""} 
+                                onChange={handleChange} 
+                                onBlur={handleBlur} 
+                                />
                                 <p className="text-danger">{errors.message && touched.message ? errors.message : ''}</p>
                                 <div className="validate" />
                             </div>
@@ -121,7 +231,13 @@ function Bookappointment(props) {
                                 <div className="sent-message">Your appointment request has been sent successfully. Thank you!</div>
                             </div>
                             <div className="text-center">
-                                <button type="submit">Make an Appointment</button>
+                                {
+                                    update ?
+                                    <button type="submit">update an Appointment</button>
+                                    :
+                                    <button type="submit">Make an Appointment</button>
+                                }
+                                
                             </div>
                         </Form>
                     </Formik>
--- src/Container/Appoinment/Listappointment.js
+++ src/Container/Appoinment/Listappointment.js
@@ -1,6 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Card, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 function Listappointment(props) {
+
+    const [data, setData] = useState([]);
+
+    const history=useHistory();
+
+    const getData = (id) => {
+        let localData = JSON.parse(localStorage.getItem("apt"));
+
+        setData(localData);
+        // console.log(LocalData);
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const handleDelete = (id) => {
+        let localData = JSON.parse(localStorage.getItem("apt"));
+
+        let fData = localData.filter((d) => d.id !== id);
+
+        localStorage.setItem("apt",JSON.stringify(fData));
+
+        getData();
+
+        console.log(fData);
+    }
+
+    const handleEdit=(id)=>{
+       
+        // let localData = JSON.parse(localStorage.getItem("apt"));
+
+        history.push("/Bookappointment" ,{id:id});
+
+        console.log(id);
+    }
+
     return (
         <main id="main">
             <section id="appointment" className="appointment">
@@ -15,6 +56,31 @@ function Listappointment(props) {
                                 <NavLink to={"/Listappointment"}>Listappointment</NavLink>
                             </div>
                         </div>
+                        {
+                            data.map((d, i) => {
+                                return (
+                                    <Card key={i}>
+                                        <CardBody>
+                                            <CardTitle tag="h5">
+                                                {d.name}
+                                            </CardTitle>
+                                            <CardSubtitle
+                                                className="mb-2 text-muted"
+                                                tag="h6"
+                                            >
+                                                {d.email}
+                                            </CardSubtitle>
+                                            <Button onClick={()=>handleEdit(d.id)}>
+                                                Edit
+                                            </Button>
+                                            <Button onClick={() => handleDelete(d.id)}>
+                                                Delete
+                                            </Button>
+                                        </CardBody>
+                                    </Card>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </section>
